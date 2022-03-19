fe14a0ec1c62939c58bd91faf0a8ec01863e59db
Tuanndq <quangtuanxyz@gmail.com>
2022-03-19 18:03:15 +0700

    add component

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import {Image} from 'react-native';
 
-export default function App() {
+import Chats from './src/screens/Chats/Chats';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import People from './src/screens/People/People';
+import Discover from './src/screens/Discover/Discover';
+import {images} from './src/images';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello anh em</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+            if (route.name === 'Chats') {
+              iconName = focused ? images.chat_active : images.chat_inactive;
+            } else if (route.name === 'People') {
+              iconName = focused
+                ? images.people_active
+                : images.people_inactive;
+            } else if (route.name === 'Discover') {
+              iconName = focused
+                ? images.discover_active
+                : images.discover_inactive;
+            }
+
+            // You can return any component that you like here!
+            return <Image source={iconName} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+          showLabel: false,
+        }}>
+        <Tab.Screen name="Chats" component={Chats} />
+        <Tab.Screen name="People" component={People} />
+        <Tab.Screen name="Discover" component={Discover} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
